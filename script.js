c16ea545b6bfc78e5c3e27cf96cdb9ec3a125ce3
GuilhermeVasconcelosDev <guilhermeprofissional33@gmail.com>
2026-09-09 22:18:14 -0300

    Update menu

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+    const menu = document.getElementById('menu-conteudo');
+    
+    // Adiciona a classe se ela não existir, ou remove se ela já existir
+    menu.classList.toggle('ativo');
+}
